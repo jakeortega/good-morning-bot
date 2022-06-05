5c91b2f7b655e7391002c5d73191e005b57beb09
Daniel Bellmas <danielb@webiya.co.il>
2022-06-05 15:16:50 +0300

    Change SLACK_TOKEN to SLACK_BOT_TOKEN

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const { isDayOff } = require('./utils/dayOff');
 
 require('dotenv').config();
 
-const { SLACK_TOKEN, SLACK_USER_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN, USER_ID } = process.env;
+const { SLACK_BOT_TOKEN, SLACK_USER_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN, USER_ID } = process.env;
 
 const app = new App({
-  token: SLACK_TOKEN,
+  token: SLACK_BOT_TOKEN,
   signingSecret: SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: SLACK_APP_TOKEN,
@@ -36,7 +36,7 @@ const sendMessage = async ({ channel = 'general', as_user = true } = {}) => {
   const gif = await fetchMorningGif();
 
   await app.client.chat.postMessage({
-    token: as_user ? SLACK_USER_TOKEN : SLACK_TOKEN,
+    token: as_user ? SLACK_USER_TOKEN : SLACK_BOT_TOKEN,
     channel,
     text: `Good morning! ${getRandomEmoji()}\n ${gif}`,
     as_user
